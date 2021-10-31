742247d95d7ff5e6ac7142551632bfc436f56036
João Barbosa <palerma@gmail.com>
2021-10-31 12:29:13 +0000

    added RNN code

---
+++ RNNet.py
@@ -0,0 +1,76 @@
+class CTRNN(nn.Module):
+    """Continuous-time RNN.
+
+    Args:
+        input_size: Number of input neurons
+        hidden_size: Number of hidden neurons
+
+    Inputs:
+        input: (seq_len, batch, input_size), network input
+        hidden: (batch, hidden_size), initial hidden activity
+    """
+
+    def __init__(self, input_size, hidden_size, dt=None, noise=1, **kwargs):
+        super().__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.tau = 100
+        if dt is None:
+            alpha = 1
+        else:
+            alpha = dt / self.tau
+        self.alpha = alpha
+        self.oneminusalpha = 1 - alpha
+
+        self.input2h = nn.Linear(input_size, hidden_size)
+        self.h2h = nn.Linear(hidden_size, hidden_size)
+        self.noise = noise
+        self.device = device
+
+    def init_hidden(self, input_shape):
+        batch_size = input_shape[1]
+
+        return torch.zeros(batch_size, self.hidden_size) 
+
+    def recurrence(self, input, hidden):
+        """Recurrence helper."""
+        pre_activation = self.input2h(input) + self.h2h(hidden) + torch.normal(mean=0, std=self.noise*torch.ones(self.hidden_size)).to(self.device)
+        h_new = torch.relu(hidden * self.oneminusalpha +
+                           pre_activation * self.alpha)
+        return h_new
+
+    def forward(self, input, hidden=None):
+        """Propogate input through the network."""
+        if hidden is None:
+            hidden = self.init_hidden(input.shape).to(input.device)
+
+        output = []
+        steps = range(input.size(0))
+        for i in steps:
+            hidden = self.recurrence(input[i], hidden)
+            output.append(hidden)
+
+        output = torch.cat(output, 0).view(input.size(0), *output[0].size())
+        return output, hidden
+
+
+class RNNNet(nn.Module):
+    """Recurrent network model.
+
+    Args:
+        input_size: int, input size
+        hidden_size: int, hidden size
+        output_size: int, output size
+        rnn: str, type of RNN, lstm, rnn, ctrnn, or eirnn
+    """
+    def __init__(self, input_size, hidden_size, output_size, noise, **kwargs):
+        super().__init__()
+
+        # Continuous time RNN
+        self.rnn = CTRNN(input_size, hidden_size, noise=noise,**kwargs)
+        self.fc = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        rnn_activity, _ = self.rnn(x)
+        out = self.fc(rnn_activity)
+        return out, rnn_activity
